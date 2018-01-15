3a508b90761981e3267543fb283febbdfbf9a4fa
quentinboudet <quentin.boudet@outlook.fr>
2018-01-15 17:15:50 +0100

    TP 03 opérationel

---
+++ src/entity/Emprunt.java
@@ -0,0 +1,89 @@
+package entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity //Indique que la classe doit être gérée par JPA
+@Table(name="emprunt") //@Table (facultatif) désigne la table à mapper 
+public class Emprunt {
+	@Id 
+	private Integer id;
+	
+	@Column(name = "DATE_DEBUT")
+	private String dateDebut;
+	
+	@Column(name = "DATE_FIN")
+	private String dateFin;
+
+	@Column(name = "DELAI")
+	private String delai;
+
+	@ManyToOne
+	@JoinColumn(name="ID_CLIENT")
+	private Client client;
+
+	@ManyToMany(mappedBy="emprunt")
+	private List<Livre> livre;
+	
+	// constructeurs
+	public Emprunt() {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(String dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public String getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(String dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	public String getDelai() {
+		return delai;
+	}
+
+	public void setDelai(String delai) {
+		this.delai = delai;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<Livre> getLivre() {
+		return livre;
+	}
+
+	public void setLivre(List<Livre> livre) {
+		this.livre = livre;
+	}
+
+}
---
+++ src/entity/Livre.java
@@ -0,0 +1,62 @@
+package entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity // Indique que la classe doit être gérée par JPA
+@Table(name = "livre") // @Table (facultatif) désigne la table à mapper
+public class Livre {
+	@Id
+	private Integer id;
+
+	@Column(name = "TITRE")
+	private String titre;
+
+	@Column(name = "AUTEUR")
+	private String auteur;
+
+	@ManyToMany
+	@JoinTable(
+			name = "compo", 
+			joinColumns = @JoinColumn(name = "ID_LIV", referencedColumnName = "ID"), 
+			inverseJoinColumns = @JoinColumn(name = "ID_EMP", referencedColumnName = "ID")
+	)
+	private List<Emprunt> emprunt;
+
+	// constructeurs
+	public Livre() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+}
